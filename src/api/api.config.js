a08968d54cefe86efc930da648aae6d352055464
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-10-28 11:07:49 +0300

    api config

---
+++ src/api/api.config.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+  baseUrl: 'hhtps://api.themoviedb.org/3/',
+  apiKey: 'c423f88e3274239684f0b71da2550fa8',
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500//${imgPath}`
+}
+
+export default apiConfig;
