b71e43eda9314308da3bac5b9e92b2113526270e
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-11-04 22:19:44 +0200

    added pages

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
+import { useParams } from 'react-router'
+
+import PageHeader from '../components/page-header/PageHeader';
+
+import { categoryType } from '../api/tmdbApi';
+
+import MovieGrid from '../components/movie-grid/MovieGrid';
+
 const Catalog = () => {
+
+  const { category } = useParams();
+
+  console.log(category);
+
   return (
-    <div>
-      Catalog
-    </div>
+    <>
+      <PageHeader>
+        {category === categoryType.movie ? 'Movies' : 'TV Series'}
+      </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
+    </>
   )
 }
 
